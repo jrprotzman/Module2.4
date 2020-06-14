b169d6dbb45f648a7bcc27d72d14f598c1b4f12a
jrprotzman <66227891+jrprotzman@users.noreply.github.com>
2020-06-13 22:13:54 -0500

    testing commiting
    Signed-off-by: jrprotzman <66227891+jrprotzman@users.noreply.github.com>

---
+++ main/camper_age_input.py
@@ -0,0 +1,8 @@
+def convert_to_months(years):
+    months = years*12
+    return months
+def age_in_years(years):
+    age_in_years = years
+
+if __name__ == '__main__':
+    print (age_in_years, "years is", convert_to_months, "months")
---
+++ test/test_functions.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def convert_to_months(12):
+        self.assertEqual(12, True(12,144))
+
+
+if __name__ == '__main__':
+    unittest.main()
